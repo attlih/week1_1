d5b7137a65fe1333ea80ed09b665857a164f28fb
attlih <atte.lihtamo@gmail.com>
2022-11-02 08:36:27 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,16 +11,17 @@ if (document.readyState !== "loading") {
 function initializeCode() {
   document.getElementById("app").innerHTML = "<h1>Hello!</h1>";
   const items = document.getElementById("item");
-  const breeds = ["A", "B", "C", "D", "E"];
-  for (const b in breeds) {
-    let item = document.createElement("item");
+  const breeds = ["A", "B", "C", "D", 'E'];
+  breeds.forEach(breed => {
+    let item = document.createElement("div");
     item.innerHTML =
       '<div class="wiki-item" >' +
       '<h1 class="wiki-header">Breed ' +
-      b +
+      breed +
       "</h1>" +
       '<div class="wiki-content"><p class="wiki-text">Some text about this breed.</p>' +
       '<div class="img-container"><img class="wiki-img" src=""></div></div></div>';
     items.appendChild(item);
-  }
+  })
 }
+
