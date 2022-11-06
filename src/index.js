11f0d381b653ac83f763f2ce91c04b425be7bf39
attlih <atte.lihtamo@gmail.com>
2022-11-06 09:48:23 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,28 +10,29 @@ if (document.readyState !== "loading") {
 
 function initializeCode() {
   document.getElementById("app").innerHTML = "<h1>DOG BREEDS</h1>";
-  getBreeds();
+  const breeds = ['affenpinscher', 'african', 'airedale', 'akita', 'appenzeller']
+  getBreeds(breeds);
 }
 
-async function getBreeds() {
+async function getBreeds(breeds) {
   const items = document.getElementById("items");
-  const url = "https://dog.ceo/api/breeds/list/all";
+  items.className = "container";
+  // const url = "https://dog.ceo/api/breeds/list/all";
   try {
-    const breedsPromise = await fetch(url);
-    const breedsJSON = await breedsPromise.json("div");
-    const breeds = Object.getOwnPropertyNames(breedsJSON.message);
+    // const breedsPromise = await fetch(url);
+    // const breedsJSON = await breedsPromise.json("div");
+    // const breeds = Object.getOwnPropertyNames(breedsJSON.message);
     console.log(breeds.slice(0, 5));
     for (const breed of breeds.slice(0, 5)) {
       let item = document.createElement("div");
-      item.className = "container";
+      item.className = "wiki-item"
       item.innerHTML =
-        `<div class="wiki-item" >
-        <h1 class="wiki-header">${breed}</h1>
+        `<h1 class="wiki-header">${breed}</h1>
         <div class="wiki-content">
         <p class="wiki-text">${await getInfo(breed)}</p>
         <div class="img-container">
         <img class="wiki-img" src=${await getImageAddress(breed)} alt=${breed} 
-        </div></div></div>`;
+        </div></div>`;
       items.appendChild(item);
     }
   } catch (err) {
@@ -40,9 +41,8 @@ async function getBreeds() {
 }
 
 async function getImageAddress(breed) {
-  const url = `https://dog.ceo/api/breed/${breed}/images/random`;
   try {
-    const imgPromise = await fetch(url);
+    const imgPromise = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
     const imgJSON = await imgPromise.json();
     return imgJSON.message;
   } catch(err) {
@@ -51,9 +51,8 @@ async function getImageAddress(breed) {
 }
 
 async function getInfo(breed) {
-  const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${breed}`;
   try {
-    const infoPromise = await fetch(url);
+    const infoPromise = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${breed}`);
     const infoJSON = await infoPromise.json();
     return infoJSON.extract;
   } catch(err) {
